9d4e25b8198380a1bf6f7b44a40424787fc10b58
Sjoerd <svanhouten@gmail.com>
2019-11-02 21:39:10 +0100

    day 9 complete

--- src/main/kotlin/com/github/shmvanhouten/adventofcode2015kt/day9/TravelingSalesMan.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2015kt/day9/TravelingSalesMan.kt
@@ -1,15 +1,76 @@
 package com.github.shmvanhouten.adventofcode2015kt.day9
 
-fun findShortestDistance(vararg pairDistances: PairDistance): Int {
-    if(pairDistances.size == 1) {
-        return pairDistances[0].distance
+import java.lang.RuntimeException
+
+fun findShortestDistance(vararg pairDistances: PairDistance): Int? {
+    return findShortestDistance(pairDistances.toList())
+}
+
+fun findShortestDistance(pairDistances: List<PairDistance>): Int? {
+    return listAllRouteDistances(pairDistances).min()
+}
+
+fun findLongestDistance(pairDistances: List<PairDistance>): Int? {
+    return listAllRouteDistances(pairDistances).max()
+}
+
+private fun listAllRouteDistances(pairDistances: List<PairDistance>): List<Int> {
+    val nodes = getDistinctNodes(pairDistances)
+    val routes = getAllRoutes(nodes)
+    val distanceMap = mapDistances(nodes, pairDistances)
+    return routes.map { getTotalDistance(it, distanceMap) }
+}
+
+fun getTotalDistance(route: List<String>, distanceMap: Map<String, Map<String, Int>>): Int {
+    return route
+        .dropLast(1)
+        .mapIndexed { i, node -> distanceMap[node]!![route[i + 1]]!! }
+        .sum()
+}
+
+fun mapDistances(nodes: List<String>, pairDistances: List<PairDistance>): Map<String, Map<String, Int>> {
+    return nodes.map { origin ->
+        origin to mapDistancesFromCurrentNodeToNodes(origin, nodes.minus(origin), pairDistances)
+    }.toMap()
+}
+
+private fun mapDistancesFromCurrentNodeToNodes(
+    origin: String,
+    otherNodes: List<String>,
+    pairDistances: List<PairDistance>
+): Map<String, Int> {
+    return otherNodes.map { destination ->
+        destination to getDistance(origin, destination, pairDistances)
+    }.toMap()
+}
+
+fun getDistance(origin: String, destination: String, pairDistances: List<PairDistance>): Int {
+    return pairDistances.find {
+        (it.destination == destination && it.origin == origin)
+                || (it.destination == origin && it.origin == destination)
+    }?.distance ?: throw RuntimeException("No distance found for $origin and $destination")
+}
+
+private fun getAllRoutes(nodes: List<String>) = findRoutes(nodes, emptyList(), emptyList())
+
+fun findRoutes(
+    nodes: List<String>,
+    routes: List<List<String>>,
+    currentRoute: List<String>
+): List<List<String>> {
+    return if (atEndOfRoute(nodes)) {
+        routes.plus(listOf(currentRoute))
     } else {
-        return pairDistances
-            .map { it.distance }
-            .sorted()
-            .dropLast(1)
-            .sum()
+        nodes.flatMap { node -> findRoutes(nodes.minus(node), routes, currentRoute.plus(node)) }
     }
+}
+
+private fun atEndOfRoute(nodes: List<String>) = nodes.isEmpty()
+
+fun getDistinctNodes(pairDistances: List<PairDistance>): List<String> {
+    return pairDistances.map { it.origin }
+        .plus(pairDistances.map { it.destination })
+        .distinct()
 }
 
 
--- src/test/kotlin/com/github/shmvanhouten/adventofcode2015kt/day9/TravelingSalesmanTest.kt
+++ src/test/kotlin/com/github/shmvanhouten/adventofcode2015kt/day9/TravelingSalesmanTest.kt
@@ -2,7 +2,6 @@ package com.github.shmvanhouten.adventofcode2015kt.day9
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.hamcrest.CoreMatchers
 import org.junit.jupiter.api.Test
 
 class TravelingSalesmanTest {
@@ -49,6 +48,36 @@ class TravelingSalesmanTest {
     }
 
     @Test
+    internal fun `between four points things can get a bit more tricky`() {
+        /*
+        Amsterdam -- 21 -- Haarlem
+            |   \       /     |
+            |    \     /      |
+            50      80        40
+            |   /       \     |
+            |  /         \    |
+        Utrecht --   19 -- Woerden
+         */
+        val amsterdamToHaarlem = PairDistance("Amsterdam", "Haarlem", 21)
+        val amsterdamToWoerden = PairDistance("Amsterdam", "Woerden", 80)
+        val amsterdamToUtrecht = PairDistance("Amsterdam", "Utrecht", 50)
+        val haarlemToUtrecht = PairDistance("Haarlem", "Utrecht", 80)
+        val haarlemToWoerden = PairDistance("Haarlem", "Woerden", 40)
+        val UtrechtToWoerden = PairDistance("Utrecht", "Woerden", 19)
+
+        val shortestDistance = findShortestDistance(
+            amsterdamToHaarlem,
+            amsterdamToWoerden,
+            amsterdamToUtrecht,
+            haarlemToUtrecht,
+            haarlemToWoerden,
+            UtrechtToWoerden
+        )
+
+        assertThat(shortestDistance, equalTo(21 + 40 + 19))
+    }
+
+    @Test
     internal fun `parse input`() {
         val pairDistances: List<PairDistance> = parseInput(testinput)
         assertThat(pairDistances.size, equalTo(3))
@@ -57,6 +86,42 @@ class TravelingSalesmanTest {
         assertThat(pairDistances[2], equalTo(PairDistance("Dublin", "Belfast", 141)))
     }
 
+    @Test
+    internal fun `part 1 and 2`() {
+        val input = """AlphaCentauri to Snowdin = 66
+AlphaCentauri to Tambi = 28
+AlphaCentauri to Faerun = 60
+AlphaCentauri to Norrath = 34
+AlphaCentauri to Straylight = 34
+AlphaCentauri to Tristram = 3
+AlphaCentauri to Arbre = 108
+Snowdin to Tambi = 22
+Snowdin to Faerun = 12
+Snowdin to Norrath = 91
+Snowdin to Straylight = 121
+Snowdin to Tristram = 111
+Snowdin to Arbre = 71
+Tambi to Faerun = 39
+Tambi to Norrath = 113
+Tambi to Straylight = 130
+Tambi to Tristram = 35
+Tambi to Arbre = 40
+Faerun to Norrath = 63
+Faerun to Straylight = 21
+Faerun to Tristram = 57
+Faerun to Arbre = 83
+Norrath to Straylight = 9
+Norrath to Tristram = 50
+Norrath to Arbre = 60
+Straylight to Tristram = 27
+Straylight to Arbre = 81
+Tristram to Arbre = 90"""
+        val pairDistances = parseInput(input)
+        println(findShortestDistance(pairDistances))
+        // 141
+        println(findLongestDistance(pairDistances))
+        // 736
+    }
 
     val testinput = """London to Dublin = 464
 London to Belfast = 518
