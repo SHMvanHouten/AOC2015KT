8b4ceb35982d808b340167c25d1baa63fe838da6
Sjoerd <svanhouten@gmail.com>
2020-02-23 18:53:14 +0100

    day 20 pt 2

--- src/main/kotlin/com/github/shmvanhouten/adventofcode2015kt/day20/HousePresents.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2015kt/day20/HousePresents.kt
@@ -1,6 +1,7 @@
 package com.github.shmvanhouten.adventofcode2015kt.day20
 
 import com.github.shmvanhouten.adventofcode2015kt.factors.factors
+import com.github.shmvanhouten.adventofcode2015kt.factors.factorsThatHaveNotBeenVisitedOver50Times
 
 fun calculatePresents(houseNumber: Long): Long {
     return factors(houseNumber)
@@ -9,6 +10,17 @@ fun calculatePresents(houseNumber: Long): Long {
 }
 
 fun firstHouseTo(number:Long) :Long {
-    return (831000L..10000000L).asSequence()
+        return (831600L..831600L).asSequence()
         .first { calculatePresents(it) >= number }
+}
+
+fun firstHouseTo2(number:Long) :Long {
+        return (776160L..1006920L).asSequence()
+        .first { calculatePresents2(it) >= number }
+}
+
+fun calculatePresents2(houseNumber: Long): Long {
+    return factorsThatHaveNotBeenVisitedOver50Times(houseNumber)
+        .map { it * 11L }
+        .sum()
 }
--- src/main/kotlin/com/github/shmvanhouten/adventofcode2015kt/factors/Factors.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2015kt/factors/Factors.kt
@@ -1,7 +1,5 @@
 package com.github.shmvanhouten.adventofcode2015kt.factors
 
-//import com.github.shmvanhouten.adventofcode2015kt.util.splitIntoTwo
-
 fun factors(number: Long): List<Long> {
     var divisor = 1L
     val factors = mutableSetOf<Long>()
@@ -17,25 +15,20 @@ fun factors(number: Long): List<Long> {
     return factors.sorted()
 }
 
-//fun factors(number: Long): List<Long> {
-//    return primeFactors(number)
-//        .listMultiplications()
-//}
-//
-//private fun List<Long>.listMultiplications(): List<Long> {
-//    return listOf(1L) +
-//            0.until(this.size)
-//                .flatMap { listMultiplications(primeFactors = this.subList(it, this.size)) }
-//                .sorted()
-//                .distinct()
-//}
-//
-//fun listMultiplications(l: Long = 1L, primeFactors: List<Long>, factors: List<Long> = emptyList()): List<Long> {
-//    if (primeFactors.isEmpty()) {
-//        return factors
-//    }
-//    val (head, tail) = primeFactors.splitIntoTwo(1)
-//    val currentPrime = head[0] * l
-//    return listMultiplications(currentPrime, tail, factors + currentPrime + tail.map { it * currentPrime })
-//}
 
+fun factorsThatHaveNotBeenVisitedOver50Times(number: Long): List<Long> {
+    var divisor = 1L
+    val factors = mutableSetOf<Long>()
+    var remainder = number
+    while (remainder >= divisor && divisor <= 50) {
+        if(number % divisor == 0L) {
+            remainder = number / divisor
+            factors += remainder
+            if(remainder <= 50) {
+                factors += divisor
+            }
+        }
+        divisor++
+    }
+    return factors.sorted()
+}
